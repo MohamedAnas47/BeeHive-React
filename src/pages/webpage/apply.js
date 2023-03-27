025d956550460c13e5e1f0e77a2c6f08e1b55858
MohamedAnas47 <mohamedanascp@gmail.com>
2023-03-27 14:38:00 +0400

    delete collapse js file

--- src/pages/webpage/apply.js
+++ src/pages/webpage/apply.js
@@ -1,12 +1,10 @@
 import Applyhead from "../../components/applyhead";
 import Footer from "../../components/footer";
-import App from "../../components/collapse";
 
 const Apply = () => {
   return (
     <div>
       <Applyhead />
-      <App />
       <Footer />
     </div>
   );
